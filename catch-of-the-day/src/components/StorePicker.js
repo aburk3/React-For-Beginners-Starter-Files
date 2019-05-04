1ce2d63a314993caf2fc250a4e199d6bcf17f7a5
aburk3 <aburk3@gmail.com>
2019-05-03 19:28:31 -0500

    Added StorePicker component and imports

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form action="" className="store-selector">
+        {/* elements must be nested inside a single parent element */}
+        <h2>Please Enter A Store</h2>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
